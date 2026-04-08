dd6f0f6f6387ec7bc5efa64ff569d9c515c8a6b8
24fmifkdidusenko-ops <24fmif.k.didusenko@std.udu.edu.ua>
2026-04-08 14:28:07 +0300

    Add files via upload

---
+++ лр8/StaticHost-6000/server.js
@@ -0,0 +1,33 @@
+import express from "express";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+import cors from "cors";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const PORT = 6001;
+
+const configPath = new URL("config.json", import.meta.url);
+const config = JSON.parse(fs.readFileSync(configPath, "utf8"));
+let mode = config.mode;
+const args = process.argv;
+const modeIndex = args.indexOf("--mode");
+if (modeIndex !== -1 && args[modeIndex + 1]) mode = args[modeIndex + 1];
+console.log(`[System] Starting ${config.appName} v.${config.version}...`);
+
+
+// if (mode === "mode1") app.use(cors());
+app.use(cors());
+if (mode === "breach") app.get("/react-mock.js", (req, res) => {
+    res.type("text/javascript").send(`alert("CRITICAL: CDN Compromised! Stealing data...");`);
+});
+
+
+app.use("/", express.static(path.join(__dirname, "public")));
+
+app.listen(PORT, () => {
+    console.log(`[System] Server started in mode "${mode}" on port ${PORT}.`);
+})
